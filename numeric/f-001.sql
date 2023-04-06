4a2de28d7f063321c6ed2f08e8346520fc80b852
Rob <110133091+popsrdg@users.noreply.github.com>
2023-04-06 17:06:46 -0400

    Added Quotes to f files
    Einstein Quotes and Franklin quotes

---
+++ numeric/f-001.sql
@@ -0,0 +1,32 @@
+Albert Einstein QUOTES
+
+Only a life lived for others is a life worthwhile.
+
+If you can't explain it simply, you don't understand it well enough.
+
+Everything should be made as simple as possible, but not simpler.
+
+We cannot solve our problems with the same thinking we used when we created them.
+
+Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning. Albert Einstein We cannot solve our problems with the same thinking we used when we created them. Albert Einstein Anyone who has never made a mistake has never tried anything new. Albert Einstein A person who never made a mistake never tried anything new. Albert Einstein Strive not to be a success, but rather to be of value. Albert Einstein You can't blame gravity for falling in love. Albert Einstein It's not that I'm so smart, it's just that I stay with problems longer. Albert Einstein
+
+Strive not to be a success, but rather to be of value.
+
+It's not that I'm so smart, it's just that I stay with problems longer. 
+
+The only source of knowledge is experience.
+
+There are two ways to live: you can live as if nothing is a miracle; you can live as if everything is a miracle.
+
+Any man who can drive safely while kissing a pretty girl is simply not giving the kiss the attention it deserves.
+
+Education is what remains after one has forgotten what one has learned in school.
+
+I know not with what weapons World War III will be fought, but World War IV will be fought with sticks and stones.
+
+
+
+
+
+
+
---
+++ numeric/f-002.sql
@@ -0,0 +1,12 @@
+Benjamin Franklin Quotes
+
+“Love your Enemies, for they tell you your Faults.”
+
+“He that lies down with Dogs, shall rise up with fleas.”
+
+“Well done is better than well said.”
+
+“Dost thou love life? Then do not squander Time; for that’s the Stuff Life is made of.”
+
+“Lost Time is never found again.”
+
