42f22d0677a063cd5b054f1f2cc0dabb60bbb28c
Rob <110133091+popsrdg@users.noreply.github.com>
2023-04-07 13:52:08 -0400

    Feature A (#2)
    two changes to code files

---
+++ snowflake queries/New feature A.sql
@@ -0,0 +1,5 @@
+This is a new file
+
+I am adding HAVERSINE(
+    just for FUNCTION
+)
--- snowflake queries/ka_file_01.sql
+++ snowflake queries/ka_file_01.sql
@@ -1,1 +1,7 @@
+
+
 select * from gsr.cds.tb_cds_customer limit 100; -- Kumar added this on 04072023
+
+
+select top 100 * from gsr.cds.tb_cds_area_group_cntry; 
+
