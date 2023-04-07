03e59e2f0234aa7474d28b312f7897ad89fb6fec
Rob <110133091+popsrdg@users.noreply.github.com>
2023-04-07 10:53:48 -0400

    first change to develop branch
    TB_CDS_TIME_PERIOD_DAY query

--- snowflake queries/Query snowflake 01.sql
+++ snowflake queries/Query snowflake 01.sql
@@ -1,2 +1,5 @@
-select top 100 * from gsr_clone.cds.tb_cds_area_group_cntry;
+--select top 100 * from gsr_clone.cds.tb_cds_area_group_cntry;
 
+
+select distinct fiscal_period, cal_mo
+from gsr.CDS.TB_CDS_TIME_PERIOD_DAY;
